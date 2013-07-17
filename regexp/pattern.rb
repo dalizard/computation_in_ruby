fc3859eac5ac2d7285b37bfd84786cc2889c24ac
Dimitar Haralanov <dimitar@forebits.com>
2013-07-17 18:10:04 +0300

    Convert Empty and Literal into NFA

--- regexp/pattern.rb
+++ regexp/pattern.rb
@@ -1,3 +1,5 @@
+require_relative '../state_machine/boot'
+
 module Pattern
   def bracket outer_precedence
     if precedence < outer_precedence
@@ -9,6 +11,10 @@ module Pattern
 
   def inspect
     "/#{self}/"
+  end
+
+  def matches? string
+    to_nfa_design.accepts? string
   end
 end
 
@@ -22,6 +28,14 @@ class Empty
   def precedence
     3
   end
+
+  def to_nfa_design
+    start_state = Object.new
+    accept_states = [start_state]
+    rulebook = NFARulebook.new []
+
+    NFADesign.new start_state, accept_states, rulebook
+  end
 end
 
 class Literal < Struct.new :character
@@ -33,6 +47,15 @@ class Literal < Struct.new :character
 
   def precedence
     3
+  end
+
+  def to_nfa_design
+    start_state = Object.new
+    accept_state = Object.new
+    rule = FARule.new start_state, character, accept_state
+    rulebook = NFARulebook.new [rule]
+
+    NFADesign.new start_state, [accept_state], rulebook
   end
 end
 
