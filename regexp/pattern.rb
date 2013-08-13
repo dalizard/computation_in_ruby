f5214f60dadb7b58cc7ea6b1877d8e3126aa7a83
Dimitar Haralanov <dimitar@forebits.com>
2013-08-13 16:11:33 +0300

    Add conversions for regext patterns to NFAs

--- regexp/pattern.rb
+++ regexp/pattern.rb
@@ -63,11 +63,26 @@ class Concatenate < Struct.new :first, :second
   include Pattern
 
   def to_s
-    [first, second].map { |pattern| pattern.bracket precedence }.join '|'
+    [first, second].map { |pattern| pattern.bracket(precedence) }.join
   end
 
   def precedence
     0
+  end
+
+  def to_nfa_design
+    first_nfa_design = first.to_nfa_design
+    second_nfa_design = second.to_nfa_design
+
+    start_state = first_nfa_design.start_state
+    accept_states = second_nfa_design.accept_states
+    rules = first_nfa_design.rulebook.rules + second_nfa_design.rulebook.rules
+    extra_rules = first_nfa_design.accept_states.map { |state|
+      FARule.new state, nil, second_nfa_design.start_state
+    }
+    rulebook = NFARulebook.new rules + extra_rules
+
+    NFADesign.new start_state, accept_states, rulebook
   end
 end
 
@@ -75,11 +90,26 @@ class Choose < Struct.new(:first, :second)
   include Pattern
 
   def to_s
-    [first, second].map { |pattern| pattern.bracket precedence }.join '|'
+    [first, second].map { |pattern| pattern.bracket(precedence) }.join '|'
   end
 
   def precedence
     0
+  end
+
+  def to_nfa_design
+    first_nfa_design = first.to_nfa_design
+    second_nfa_design = second.to_nfa_design
+
+    start_state = Object.new
+    accept_states = first_nfa_design.accept_states + second_nfa_design.accept_states
+    rules = first_nfa_design.rulebook.rules + second_nfa_design.rulebook.rules
+    extra_rules = [first_nfa_design, second_nfa_design].map { |nfa_design|
+      FARule.new start_state, nil, nfa_design.start_state
+    }
+    rulebook = NFARulebook.new rules + extra_rules
+
+    NFADesign.new start_state, accept_states, rulebook
   end
 end
 
@@ -92,5 +122,19 @@ class Repeat < Struct.new :pattern
 
   def precedence
     2
+  end
+
+  def to_nfa_design
+    pattern_nfa_design = pattern.to_nfa_design
+
+    start_state = Object.new
+    accept_states = pattern_nfa_design.accept_states + [start_state]
+    rules = pattern_nfa_design.rulebook.rules
+    extra_rules = pattern_nfa_design.accept_states.map { |accept_state|
+      FARule.new accept_state, nil, pattern_nfa_design.start_state
+    } + [ FARule.new(start_state, nil, pattern_nfa_design.start_state) ]
+    rulebook = NFARulebook.new rules + extra_rules
+
+    NFADesign.new start_state, accept_states, rulebook
   end
 end
